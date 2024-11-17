62a0bc1f28ea9dac6b226b04ec362120eef72eab
Maria <salyginamaria156@gmail.com>
2024-11-17 23:26:51 +0300

    настройка webpack  и babel

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 // import { StrictMode } from 'react'
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App/";
+import App from "./App";
 
 import"./config/configureMobX"
 import "./styles/styles.scss";
@@ -15,3 +16,7 @@ createRoot(document.getElementById("root")!).render(
   </BrowserRouter>
   /* </StrictMode>, */
 );
+
+if(module.hot) {
+  module.hot.accept()
+}
