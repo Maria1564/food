ad10da7b44f4aba29bdc0082c61170f9c7998595
Maria <salyginamaria156@gmail.com>
2024-11-21 22:31:21 +0300

    начало разработки компонента FavoriteRecipes

--- src/App/pages/FavoriteRecipes/FavoriteRecipes.tsx
+++ src/App/pages/FavoriteRecipes/FavoriteRecipes.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import Text from "components/Text";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+import s from "./FavoriteRecipes.module.scss";
 
+const FavoriteRecipes: React.FC = () => {
+  const navigate = useNavigate();
+  const [list, setList] = useState([]);
 
-const FavoriteRecipes = () => {
+  useEffect(() => {
+    const favorites = localStorage.getItem("listFavoritesRecipes");
+    if (favorites) {
+      setList(JSON.parse(favorites));
+    }
+  }, []);
+
   return (
-    <div>FavoriteRecipes</div>
-  )
-}
+    <div className={s.favorite}>
+      <div className="container">
+        <Text tag="h1" view="title" className={s.favorite__title}>
+          Favorites
+        </Text>
+      </div>
+    </div>
+  );
+};
 
-export default FavoriteRecipes
+export default FavoriteRecipes;
