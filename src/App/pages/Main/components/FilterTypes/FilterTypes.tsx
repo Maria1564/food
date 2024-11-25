43bb9fbc740a43107b2796e74745083a46be3280
maria <salyginamaria156@gmail.com>
2024-11-25 11:36:28 +0300

    удалены ненужные строки

--- src/App/pages/Main/components/FilterTypes/FilterTypes.tsx
+++ src/App/pages/Main/components/FilterTypes/FilterTypes.tsx
@@ -14,45 +14,25 @@ import { TypeMealStore } from 'store/TypeMealStore'
 
 
 const FilterTypes: React.FC = () => {
-  const [arrOptions, setArrOptions] = useState(localStorage.getItem("selectOptions"));
+  const [arrOptions] = useState(localStorage.getItem("selectOptions"));
   const [searchParams, setSearchParams] = useSearchParams();
   const objContext = useContext(ParamsContext);
   const options = useLocalStore(()=> new TypeMealStore())
 
   
   const handlerGetTitle = useCallback(() => {
-    // const parsedOptions = arrOptions ? JSON.parse(arrOptions) : [];
-    console.log(options.listMeal)
     return getTitle(options.listMeal);
   }, [arrOptions])
 
   const handlerChange  = useCallback((value: OptionsType): void => {
-    // localStorage.setItem("selectOptions", JSON.stringify(value))
-    // options.setNewTypeMeal(value)
-
 
     const query = searchParams.get("query") || ""
     const page = searchParams.get("page")
     const offset = (Number(page) - 1) * 9
+    objContext?.handlerQueryParams(offset, Number(page), query)
     
     setSearchParams((prev) => {
-      // const params = new URLSearchParams(prev);
-      // const selectOptions = localStorage.getItem("selectOptions");
-      
-      // if (selectOptions !== null) {
-        //     const options = JSON.parse(selectOptions);
-        //     if(options.length === 0){
-          //       params.delete("type");
-          //       return params
-          //     }
-          //     if (options.length > 0 && options[0].value) {
-            //         params.set("type", options[0].value);
-            //     }
-            // }
-            console.log(options.setNewTypeMeal(value).get("type"))
-            objContext?.handlerQueryParams(offset, Number(page), query)
-            return options.setNewTypeMeal(value)
-            
+      return options.setNewTypeMeal(value)
   });
 
   }, [objContext, searchParams, setSearchParams])
